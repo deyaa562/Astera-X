3ce013ccf3f0ad89d4561944fe4afad426b08938
deyaa562 <56220791+deyaa562@users.noreply.github.com>
2021-08-07 22:31:41 +0300

    Add files via upload

---
+++ __Scripts/JumpsGT.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+
+public class JumpsGT : MonoBehaviour 
+{
+	private static PlayerShip	_Player;
+
+	private static int			_Jumps;
+	[Header("Assign in the inspector")]
+	public static Text			_JumpsText;
+
+	private void Awake()
+    {
+		_Player = null;
+		_Jumps = 3;
+		_JumpsText.text = _Jumps.ToString();
+    }
+
+
+	/// <summary>
+	/// <para>Decreases the number of jumps remaining and moves the playership to a safe zone.</para>
+	/// <para>If there are no more jumps remaining the scene will be reloaded after 4 seconds </para>
+	/// </summary>
+	/// <param name="player">PlayerShip</param>
+	public static void Jump(GameObject player)
+    {
+		if(_Jumps > 0)
+        {
+			if(_Player == null)
+            {
+                try
+                {
+					_Player = player.GetComponent<PlayerShip>();
+                } 
+				catch(NullReferenceException e)
+                {
+					Debug.LogWarning(e);
+                }
+            }
+
+			_Jumps--;
+			_JumpsText.text = _Jumps.ToString();
+			_Player.Move2SafeZone();
+        }
+        else
+        {
+			
+			ReloadScene();
+        }
+    }
+
+	private static void ReloadScene()
+    {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+	
+
+
+	
+}
---
+++ __Scripts/ScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class ScoreManager : MonoBehaviour {
+
+	private static int		_Score;
+
+	[SerializeField]
+	static Text				_ScoreText;
+
+	// Use this for initialization
+	void Start () 
+	{
+		_Score = 0;
+	}
+	
+
+	/// <summary>
+	/// <para>Accesses the AsteroidsScriptableObject in the AsteraX.c# and gets
+	/// the score that should be added according to the size of the destroyed asteroid.</para>
+	/// </summary>
+	/// <param name="size">Asteroid size</param>
+	public static void AddScore(int size)
+    {
+		_Score = _Score + AsteraX.AsteroidsSO.pointsForAsteroidSize[size];
+		//_ScoreText.text = _Score.ToString();
+		Debug.Log("Score is:" + _Score.ToString());
+    }
+}
